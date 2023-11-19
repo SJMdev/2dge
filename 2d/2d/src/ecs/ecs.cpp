0af74d98fd128f80c9f8dcc075b2cba5946c8203
ere erer <sjorsmallon@gmail.com>
2023-11-19 14:03:20 +0100

    [feat] more work on ECS

--- 2d/2d/src/ecs/ecs.cpp
+++ 2d/2d/src/ecs/ecs.cpp
@@ -1,2 +1,23 @@
 #include "ecs.hpp"
 
+int Entity::GetId() const
+{
+	return id;
+}
+
+
+// should we check if the entity does not already exist?
+void System::addEntityToSystem(Entity entity) {
+	entities.push_back(entity);
+}
+void System::removeEntityFromSystem(Entity entity) {
+	entities.erase(std::remove(entities.begin(), entities.end(), entity), entities.end());
+}
+std::vector<Entity> System::GetSystemEntities() const
+{
+	return entities;
+}
+const Signature& System::getComponentSignature() const
+{
+	return componentSignature;
+}
--- 2d/2d/src/ecs/ecs.hpp
+++ 2d/2d/src/ecs/ecs.hpp
@@ -1,16 +1,71 @@
 #pragma once
+#include <bitset>
 
-class Component {
+const unsigned int MAX_COMPONENTS = 32;
+typedef std::bitset<MAX_COMPONENTS> Signature;
+// we use a bitset| (1s and 0s) to kee ptrack of which components an entity has,
+// which also helps keep track of which entities a system is interested in.
 
+struct BaseComponent {
+protected:
+	static int nextId;
+};
+
+// used to assign a unique ID to a component type
+template<typename T> 
+struct Component: BaseComponent
+{
+	// returns the unqiue ID of the component<T> (uh, if we call this multiple times,
+	// we will just increase the next id?
+	// NO, since static gets initialized once. This is super convoluted!
+	static int getId() {
+		static auto id = nextId++;
+		return id;
+	}
 };
 
 class Entity {
+private:
+	int id;
+
+public:
+	Entity(int id)
+	:
+		id(id)
+	{
+
+	}
+	~Entity() = default;
+	int GetId() const;
 
 };
 
 class System {
+	// which components an entity must have for the system to consider the entity.
+	Signature componentSignature;
+	// list of all the entities that the System is interested in.
+	std::vector<Entity> entities;
 
+public:
+	System() = default;
+	~System() = default;
+	void addEntityToSystem(Entity entity);
+	void removeEntityFromSystem(Entity entity);
+	std::vector<Entity> GetSystemEntities() const;
+	const Signature& getComponentSignature() const;
+	
+	// define the component type T that entities must have to be considered
+	// by the system.
+	// "RequireComponent" is a horrible name. It should be like: add_component_to_signature
+	// or something like that.
+	template <typename TComponent> void RequireComponent();
 };
+
+template <typename TComponent> void System::RequireComponent() {
+	const auto componentId = Component<TComponent>::GetId();
+	componentSignature.set(componentId);
+}
+
 
 class Registry {
 
